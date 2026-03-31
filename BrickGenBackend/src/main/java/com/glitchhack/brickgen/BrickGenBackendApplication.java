e2dff4bcebb695bfbaaf625a16db964388c12bca
Chris Valle <christophervalle3338@gmail.com>
2026-03-30 19:23:28 -0700

    initial commit for java backend

---
+++ BrickGenBackend/src/main/java/com/glitchhack/brickgen/BrickGenBackendApplication.java
@@ -0,0 +1,14 @@
+package com.glitchhack.brickgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickGenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrickGenBackendApplication.class, args);
+        System.out.println("Something");
+    }
+
+}
---
+++ BrickGenBackend/src/test/java/com/glitchhack/brickgen/BrickGenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glitchhack.brickgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickGenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
